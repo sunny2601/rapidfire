7dd4a3ee28c79d6692e79d2ad096c3dcbb1fd36c
sunny2601 <sunnysarrot@gmail.com>
2022-05-31 12:11:57 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rapidfire::Engine.routes.draw do
-  resources :surveys do
+  resources :surveys,, :except => [:show] do
     get 'results', on: :member
     resources :questions
     resources :attempts, only: [:new, :create, :edit, :update, :show]
