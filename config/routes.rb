aa15e85fc8bd87e8e3c377761a076706d19f8f93
sunny2601 <sunnysarrot@gmail.com>
2022-05-31 11:39:27 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rapidfire::Engine.routes.draw do
   resources :surveys do
     get 'results', on: :member
+    get 'global_parameters', as: 'survey_global_parameter'
 
     resources :questions
     resources :attempts, only: [:new, :create, :edit, :update, :show]
